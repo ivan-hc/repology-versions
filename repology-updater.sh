ff2d33fab54693ecdb9d3bc7e051015c2b994187
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-03-19 20:50:06 +0100

    Add files via upload

---
+++ repology-updater.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+[ ! -f ./main.zip ] && curl -#Lo main.zip "https://github.com/ivan-hc/AM/archive/main.zip" && unzip -qq main.zip
+if wget -q --tries=10 --timeout=20 --spider https://repology.org; then
+	rm -f ./versions-list
+	for arg in AM-main/programs/x86_64/*; do
+		if ! grep -q "api.github.com\|download.kde.org\|ffwa-" "$arg" && grep -q "^wget.*http" "$arg"; then
+			appname=$(echo "$arg" | sed -- 's:.*/::')
+			purearg=$(echo "$appname" | sed -- 's/-electron$//g; s/-host$//g; s/-appimage$//g; s/-app$//g; s/inkscape-next$/inkscape-dev/g')
+			version=$(wget -q -O - "https://repology.org/project/$purearg/versions" | grep -i "version" | grep -i "new" | head -1 | tr '><' '\n' | grep "^[0-9]") || version=""
+			if [ -z "$version" ]; then
+				version=$(wget -q -O - "https://repology.org/project/$purearg/versions" | grep -i "version" | grep -i "uniq" | head -1 | tr '><' '\n' | grep "^[0-9]") || version=""
+			fi
+			if [ -z "$version" ]; then
+				version=$(wget -q -O - "https://repology.org/project/$purearg-client/versions" | grep -i "version" | grep -i "new" | head -1 | tr '><' '\n' | grep "^[0-9]") || version=""
+			fi
+			if [ -z "$version" ]; then
+				version=$(wget -q -O - "https://repology.org/project/$purearg-client/versions" | grep -i "version" | grep -i "uniq" | head -1 | tr '><' '\n' | grep "^[0-9]") || version=""
+			fi
+			if [ -z "$version" ]; then
+				version=$(wget -q -O - "https://repology.org/project/$purearg-desktop/versions" | grep -i "version" | grep -i "new" | head -1 | tr '><' '\n' | grep "^[0-9]") || version=""
+			fi
+			if [ -z "$version" ]; then
+				version=$(wget -q -O - "https://repology.org/project/$purearg-desktop/versions" | grep -i "version" | grep -i "uniq" | head -1 | tr '><' '\n' | grep "^[0-9]") || version=""
+			fi
+			if [ -z "$version" ]; then
+				version="unknown"
+			fi
+			echo "◆ $appname $version" >> ./versions-list
+		fi
+	done
+fi
